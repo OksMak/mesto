fab62cd4a2dc15ebc5889505b0f5cb5dcea0e23e
oks <oksana.maklagina98@gmail.com>
2023-04-19 20:45:57 +0300

    feat: добавить загрузку карточек на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,72 @@
-let buttonEdit = document.querySelector('.profile__button-edit');
-let buttonClose = document.querySelector('.popup__close');
-let popup = document.querySelector('.popup');
-let inputName = document.querySelector('.popup__input_type_name');
-let inputProfession = document.querySelector('.popup__input_type_profession');
-let profileName = document.querySelector('.profile__name');
-let profileProfession = document.querySelector('.profile__speciality');
-let form = document.querySelector('.popup__form');
+// profile
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__speciality');
+const buttonEdit = document.querySelector('.profile__button-edit');
+
+// popup
+
+const popup = document.querySelector('.popup');
+const inputName = document.querySelector('.popup__input_type_name');
+const inputProfession = document.querySelector('.popup__input_type_profession');
+const buttonClose = document.querySelector('.popup__close');
+const form = document.querySelector('.popup__form');
+
+// template gallery-item
+
+const cardTemplate = document.querySelector('#gallery__list-item').content;
+
+// gallery-list
+
+const galleryList = document.querySelector('.gallery__list');
+
+// cards
+
+const initialCards = [
+  {
+    name: 'Байкал',
+    link: 'https://images.unsplash.com/photo-1552735855-79278b1667ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=385&q=80'
+  },
+  {
+    name: 'Эльбрус',
+    link: 'https://images.unsplash.com/photo-1638989420853-a6437f7a0d2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://images.unsplash.com/photo-1557094005-176cbfe3554d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1334&q=80'
+  },
+  {
+    name: 'Красная Поляна',
+    link: 'https://images.unsplash.com/photo-1627110281873-696878f72bc3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1175&q=80'
+  },
+  {
+    name: 'Ласточкино гнездо',
+    link: 'https://images.unsplash.com/photo-1598867957922-2cd433a5dacc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
+  },
+  {
+    name: 'Алтай',
+    link: 'https://images.unsplash.com/photo-1593948360735-4ddb6f0dde7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  }
+]; 
+
+const createCard = (data) => {
+  const cardElement = cardTemplate.querySelector('.gallery__list-item').cloneNode(true);
+  const cardName = data.name;
+  const cardLink = data.link;
+
+  const cardImage = cardElement.querySelector('.gallery__list-image');
+  const cardTitle = cardElement.querySelector('.gallery__image-title');
+
+  cardImage.src = cardLink;
+  cardImage.alt = cardName;
+  cardTitle.textContent = cardName;
+
+  galleryList.prepend(cardElement);
+}
+
+initialCards.forEach(item => {
+  createCard(item);
+})
+
 
 function openPopup () {
   if (!popup.classList.contains('popup_opened')) {
